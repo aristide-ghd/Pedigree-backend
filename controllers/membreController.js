b2d03146131fdbaf8eebeeaeb1138e9edc715a3a
aristide <ari-luxury@gmail.com>
2024-07-24 19:29:42 +0100

    recupation des membres par sexe, type de lien et mise à jour du doc api

--- controllers/membreController.js
+++ controllers/membreController.js
@@ -32,11 +32,56 @@ const ajouterMembre = async (req, res) => {
 // Fonction pour afficher tous les membres pour un utilisateur
 const getTousMembres = async (req, res) => {
   const idUtilisateur = req.user.identity._id
+  //Recupere les params de requete si ils sont fournis
+  const {sexe, type_de_lien} = req.query;
   try {
-    const membres = await Membre.find({ id_user: idUtilisateur});
+
+    //Assure la recuperation des membres de user connecté
+    let filtre = {id_user: idUtilisateur};
+
+    //Ajoute le paramètre 'sexe' à l'objet filtre s'il est fourni
+    if(sexe)
+    {
+      filtre.sexe = sexe;
+    }
+
+    // Vérifie si un type de lien a été fourni dans les paramètres de la requête.
+    if(type_de_lien)
+    {
+      // Lien.find cherche dans Lien tous les documents qui ont le type_de_lien spécifié et appartiennent à l'utilisateur.
+      // La projection 'id_membre' indiquant la recuperation des champs id_membre des documents trouvés.
+      const liens = await Lien.find({ type_de_lien, id_user: idUtilisateur}, 'id_membre');
+
+      // Crée un tableau idmembres contenant uniquement les id_membre des liens trouvés
+      const idmembres = liens.map(lien => lien.id_membre);
+
+      // Ajoute un filtre pour ne récupérer que les membres dont l'ID est dans le tableau idsMembres
+      filtre._id = { $in: idmembres };
+    }
+
+    //Requete pour obtenir les membres avec le filtre
+    const membres = await Membre.find(filtre);
     res.status(201).json(membres);
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(400).json({ message: "Aucun membre disponible" });
+  }
+};
+
+// Fonction pour afficher un membre d'un utilisateur
+const AfficherMembreParId = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    console.log(req.body);
+    const membre = await Membre.findOne({_id:req.params.id},{_id: false });
+    console.log(membre);
+    
+    if(!membre) {
+      return res.status(400).json({ message: "Membre non trouvé" });
+    }
+    res.status(201).json({ message: "Membre trouvé avec succès", data: membre });
+  }
+  catch (err) {
+    res.status(400).json({ message: "Erreur de lecture"});
   }
 };
 
@@ -88,4 +133,4 @@ const getMembreParSexe = async (req, res) => {
 //   }
 // };
 
-module.exports = { ajouterMembre, getTousMembres, modifierMembreParId, getMembreParSexe };
+module.exports = { ajouterMembre, getTousMembres, modifierMembreParId, getMembreParSexe, AfficherMembreParId };
--- routes/membreRoutes.js
+++ routes/membreRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { authMiddleware } = require('../middleware/authMiddleware');
-const { ajouterMembre, getTousMembres, modifierMembreParId, getMembreParSexe } = require('../controllers/membreController');
+const { ajouterMembre, getTousMembres, modifierMembreParId, getMembreParSexe, AfficherMembreParId } = require('../controllers/membreController');
 const { yupValidator } = require('../middleware/yup');
 const {addMembreDto} = require('../dto/addmembreDto');
 
 //Route pour ajouter un nouveau membre
 router.post('/ajouter', authMiddleware, yupValidator(addMembreDto) ,ajouterMembre);
 
-//Route pour afficher tous les membres pour un utilisateur
+//Route pour afficher tous les membres pour un utilisateur par sexe et par type de lien
 router.get('/tous', authMiddleware, getTousMembres);
 
 //Route pour modifier un membre d'un utilisateur par l'ID
 router.put('/modifier/:id', authMiddleware, modifierMembreParId);
+
+//Route pour afficher un membre d'un utilisateur par l'ID
+router.get('/afficher/:id', authMiddleware, AfficherMembreParId);
 
 //Route pour recupérer les membres par sexe pour un utilisateur
 router.get('/recuperer/:sexe', authMiddleware, getMembreParSexe);
