a767494afc48fc3b133e17133dffdd4ef748878f
Cyr KOTY <cyrh2@kali>
2024-07-29 09:49:34 +0100

    fixed: errors in the family controller file

--- controllers/family_controller.js
+++ controllers/family_controller.js
@@ -1,7 +1,5 @@
 const Family = require('../models/family.js');
 const {checkFamilyExist} = require('../middleware/family_middleware.js');
-const family = require('../models/family.js');
-const Membre = require('../models/membre');
 
 //A function to create a new family for a user
 
@@ -12,12 +10,12 @@ const create_famliy = async (req, res) => {
         await checkFamilyExist(req, res, async() => {
             //save the Family info receive by the frontend if it's not already existing
             const newFamille = new Family(body);
-            const newfamily =  await newFamille.save();
-            //if the it's a new family the id of the user is used as the creator_id
-            const idCreator = req.user.identity._id;
-            body.id_creator = idCreator;
+            await newFamille.save();
+            // //if the it's a new family the id of the user is used as the creator_id
+            // const idCreator = req.user.identity._id;
+            // body.id_creator = idCreator;
         });
-        res.status(201).json({Message: "Famille  ajouter avec sucès"});
+        res.status(201).json({Message: "Famille créer avec sucès"});
     } catch(err) {
         res.status(400).json({Message: "Une erreur est survenu lors de la creation de la famille"});
     }
